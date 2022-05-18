72879f7002a05c9249d4b1c065c900b2e7c89b87
RamonaS2 <bandeiraramona2@gmail.com>
2022-05-18 13:57:38 -0300

    requisito 3 e 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,34 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import { FavoritePokemons } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <FavoritePokemons.js />', () => {
+  it(`Se é exibida na tela a mensagem No favorite pokemon found,
+  caso a pessoa não tenha pokémons favoritos.`, () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const semFavorito = screen.getByText('No favorite pokemon found');
+
+    expect(semFavorito).toBeInTheDocument();
+  });
+
+  it('Se são exibidos todos os cards de pokémons favoritados.', () => {
+    renderWithRouter(<App />);
+
+    const detalhe = screen.getByRole('link', { name: 'More details' });
+
+    userEvent.click(detalhe);
+    const det = screen.getByRole('heading', { name: 'Pikachu Details', level: 2 });
+    expect(det).toBeInTheDocument();
+
+    const verifyFavoritos = screen.getByRole('checkbox', { name: 'Pokémon favoritado?' });
+    expect(verifyFavoritos).toBeInTheDocument();
+
+    userEvent.click(verifyFavoritos);
+    const img = screen.getByRole('img', { name: 'Pikachu is marked as favorite' });
+    expect(img).toBeInTheDocument();
+  });
+});
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,25 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../components';
+import renderWithRouter from './renderWithRouter';
+
+describe('Teste o componente <NotFound.js />', () => {
+  it('Se a página contém um heading h2 com o texto Page requested not found 😭', () => {
+    renderWithRouter(<NotFound />);
+
+    const titulo = screen.getByRole(
+      'heading', { name: /Page requested not found/i, level: 2 },
+    );
+    expect(titulo).toBeInTheDocument();
+  });
+
+  it('Se a página mostra a imagem correta', () => {
+    renderWithRouter(<NotFound />);
+
+    const imagem = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+    const img = screen.getByRole('img',
+      { name: 'Pikachu crying because the page requested was not found' });
+
+    expect(img.src).toBe(imagem);
+  });
+});
